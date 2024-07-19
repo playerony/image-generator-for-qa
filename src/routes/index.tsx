b1aa2a9fd89eaf96555d6645679b724b577b8102
playerony <pawel.wojtasinski.1995@gmail.com>
2024-07-19 16:16:55 +0200

    chore: update styles and store connection

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,33 +1,84 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-const Home = () => (
-  <div class="image-generator">
-    <h2 class="image-generator__title">
-      <span class="image-generator__title-span">IMAGE</span>
-      GENERATOR
-    </h2>
-    <div class="image-generator__ratio-inputs">
-      <input type="number" id="ratio-width" value="16" class="image-generator__input image-generator__input--ratio-width" />
-      <input type="number" id="ratio-height" value="9" class="image-generator__input image-generator__input--ratio-height" />
-    </div>
-    <div class="image-generator__dimension-inputs">
-      <div class="image-generator__input-group">
-        <label for="width" class="image-generator__label">Width:</label>
-        <input type="number" id="width" value="1280" class="image-generator__input image-generator__input--width" />
+const Home = component$(() => {
+  const state = useStore({
+    width: 1280,
+    height: 720,
+    outputSize: 1,
+    ratioWidth: 16,
+    ratioHeight: 9,
+  });
+
+  return (
+    <div class="image-generator">
+      <h2 class="image-generator__title">
+        IMAGE
+        <span class="image-generator__title-span">GENERATOR</span>
+      </h2>
+      <div class="image-generator__ratio-inputs">
+        <div class="image-generator__input-group">
+          <label for="ratio-width" class="image-generator__label">
+            Ratio Width
+          </label>
+          <input
+            type="number"
+            id="ratio-width"
+            value={state.ratioWidth}
+            class="image-generator__input image-generator__input--ratio-width"
+          />
+        </div>
+        <div class="image-generator__input-group">
+          <label for="ratio-height" class="image-generator__label">
+            Ratio Height
+          </label>
+          <input
+            type="number"
+            id="ratio-height"
+            value={state.ratioHeight}
+            class="image-generator__input image-generator__input--ratio-height"
+          />
+        </div>
+      </div>
+      <div class="image-generator__dimension-inputs">
+        <div class="image-generator__input-group">
+          <label for="width" class="image-generator__label">
+            Width
+          </label>
+          <input
+            id="width"
+            type="number"
+            value={state.width}
+            class="image-generator__input image-generator__input--width"
+          />
+        </div>
+        <div class="image-generator__input-group">
+          <label for="height" class="image-generator__label">
+            Height
+          </label>
+          <input
+            id="height"
+            type="number"
+            value={state.height}
+            class="image-generator__input image-generator__input--height"
+          />
+        </div>
       </div>
       <div class="image-generator__input-group">
-        <label for="height" class="image-generator__label">Height:</label>
-        <input type="number" id="height" value="720" class="image-generator__input image-generator__input--height" />
+        <label for="output-size" class="image-generator__label">
+          Estimated output size in megabytes
+        </label>
+        <input
+          type="number"
+          id="output-size"
+          value={state.outputSize}
+          class="image-generator__input image-generator__input--output-size"
+        />
       </div>
+      <button class="image-generator__generate-button">Generate</button>
     </div>
-    <div class="image-generator__output-size">
-      <label for="output-size" class="image-generator__label">Output size in megabytes:</label>
-      <input type="number" id="output-size" value="1" class="image-generator__input image-generator__input--output-size" />
-    </div>
-    <button class="image-generator__generate-button">Generate</button>
-  </div>
-);
+  );
+});
 
 export const head: DocumentHead = {
   title: "Welcome to Qwik",
@@ -39,4 +90,4 @@ export const head: DocumentHead = {
   ],
 };
 
-export default component$(Home);
+export default Home;
